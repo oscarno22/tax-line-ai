7a7dcd04fadc771e24cee5c0c81144032a652d23
Oscar Nolen <oscarnolen@gmail.com>
2026-04-29 20:28:47 -0400

    setting up lambda layer for installing dependencies

--- src/handler.py
+++ src/handler.py
@@ -1,4 +1,5 @@
 import json
+import requests
 
 
 def lambda_handler(event, context):
@@ -8,6 +9,12 @@ def lambda_handler(event, context):
         return {
             "statusCode": 200,
             "body": json.dumps({"status": "ok"}),
+        }
+
+    if route == "GET /joke":
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"requests_version": requests.__version__}),
         }
 
     return {"statusCode": 404, "body": json.dumps({"error": "Not found"})}
